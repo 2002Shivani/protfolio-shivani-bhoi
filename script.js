604f5cb40828b5767714089f706c79c50710a7bd
2002Shivani <99312719+2002Shivani@users.noreply.github.com>
2025-04-15 11:16:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+AOS.init();
+
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thanks for contacting, Shivani will get back to you soon!");
+});
